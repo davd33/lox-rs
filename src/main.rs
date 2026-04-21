58affb60985a611987c5445e7fdda38b6b1d1f04
David Rueda <davd33@gmail.com>
2026-04-21 18:46:03 +0200

    Lox scanner

---
+++ src/main.rs
@@ -0,0 +1,138 @@
+use derive_more::Display;
+
+struct Scanner {
+	source: String,
+	current: usize,
+	start: usize,
+	line: usize,
+	tokens: Vec<Token>,
+}
+
+impl Scanner {
+	fn new(source: String) -> Self {
+		Scanner {
+			source,
+			current: 0,
+			start: 0,
+			line: 1,
+			tokens: vec![],
+		}
+	}
+
+	fn advance(&mut self) -> Option<char> {
+		let c = self.source[self.current..].chars().next();
+		if let Some(c) = c {
+			self.current += c.len_utf8();
+		}
+		c
+	}
+
+	fn add_token(&mut self, _type: TokenType) {
+		self.add_token_literal(_type, None);
+	}
+
+	fn add_token_literal(&mut self, _type: TokenType, literal: Option<Literal>) {
+		self.tokens.push(
+			Token::new(_type,
+			           self.source[self.start..self.current].to_string(),
+			           literal,
+			           self.line));
+	}
+
+	fn scan_token(&mut self) {
+		if let Some(c) = self.advance() {
+			match c {
+				'(' => self.add_token(TokenType::LeftParen),
+				')' => self.add_token(TokenType::RightParen),
+				'{' => self.add_token(TokenType::LeftBrace),
+				'}' => self.add_token(TokenType::RightBrace),
+				',' => self.add_token(TokenType::Comma),
+				'.' => self.add_token(TokenType::Dot),
+				'-' => self.add_token(TokenType::Minus),
+				'+' => self.add_token(TokenType::Plus),
+				';' => self.add_token(TokenType::Semicolon),
+				'*' => self.add_token(TokenType::Star),
+				_ => self.add_token(TokenType::Unknown),
+			}
+		}
+	}
+
+	fn is_at_end(&self) -> bool {
+		self.current >= self.source.chars().count()
+	}
+
+	pub fn scan_tokens(&mut self) -> Vec<Token> {
+		println!("Scanning Lox code: \n{}", &self.source);
+		let mut result = vec![];
+
+		while !self.is_at_end() {
+			self.start = self.current;
+			self.scan_token();
+		}
+
+		result.push(Token::new(TokenType::Eof, String::new(), None, 0,));
+
+		result
+	}
+}
+
+#[derive(Display)]
+enum TokenType {
+	// Single-character tokens.
+	LeftParen, RightParen, LeftBrace, RightBrace,
+	Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+
+	// One or two character tokens.
+	Bang, BangEqual,
+	Equal, EqualEqual,
+	Greater, GreaterEqual,
+	Less, LessEqual,
+
+	// Literals.
+	Identifier, String, Number,
+
+	// Keywords.
+	And, Class, Else, False, Fun, For, If, Nil, Or,
+	Print, Return, Super, This, True, Var, While,
+
+	Eof, Unknown
+}
+
+struct Literal;
+
+#[derive(Display)]
+#[display("{line:} {lexeme} ({_type})")]
+struct Token {
+	_type: TokenType,
+	lexeme: String,
+	literal: Option<Literal>,
+	line: usize,
+}
+
+impl Token {
+	pub fn new(_type: TokenType,
+	           lexeme: String,
+	           literal: Option<Literal>,
+	           line: usize) -> Self {
+
+		Token {
+			_type,
+			lexeme,
+			literal,
+			line,
+		}
+	}
+}
+
+fn run(source: String) {
+	let mut scanner = Scanner::new(source);
+	let tokens = scanner.scan_tokens();
+
+	for t in tokens.iter() {
+		println!("{t}");
+	}
+}
+
+fn main() {
+	run("print 'hello world'".to_string());
+}
